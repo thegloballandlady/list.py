5b66f58b4f83a51fbcfbfa3dc897f76fc33168c5
thegloballandlady <vidaabuor1@gmail.com>
2024-03-16 23:09:54 +0100

    add new file

---
+++ list.py
@@ -0,0 +1,20 @@
+my_list = []
+
+my_list.extend([10, 20, 30, 40])
+
+my_list.insert(1, 15)
+
+my_list.extend([50, 60, 70])
+
+my_list.pop()
+
+my_list.sort()
+
+index_30 = my_list.index(30)
+
+print(f"The index of 30 in my_list is: {index_30}")
+
+
+print("Updated my_list:", my_list)
+
+
